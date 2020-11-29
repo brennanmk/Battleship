f8fdde1a2c777a29473fa489d9bd70234865f94c
Brennan <brennanmk2200@gmail.com>
2020-11-29 14:25:27 -0500

    Fixed battleship location creation

--- player.py
+++ player.py
@@ -1,67 +1,70 @@
 import ships
 
-class player:       
+
+class player:
     def __init__(self):
-        self.board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-        self.playerArsenal  = []
-        self.playerArsenal.append(ships.carrier(0,0,0,0))
-        self.playerArsenal.append(ships.battleShip(0,0,0,0))
-        self.playerArsenal.append(ships.cruiser(0,0,0,0))
-        self.playerArsenal.append(ships.submarine(0,0,0,0))
-        self.playerArsenal.append(ships.destroyer(0,0,0,0))
+        self.board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        self.playerArsenal = []
+        self.playerArsenal.append(ships.carrier(0, 0, 0, 0))
+        self.playerArsenal.append(ships.battleShip(0, 0, 0, 0))
+        self.playerArsenal.append(ships.cruiser(0, 0, 0, 0))
+        self.playerArsenal.append(ships.submarine(0, 0, 0, 0))
+        self.playerArsenal.append(ships.destroyer(0, 0, 0, 0))
 
     def printEnemyMap(self):
-      print("\n-------------------------------------")
-      for row in self.board:
-         for collumns in row:
-            if collumns == 1:
-               print("%s X" % ("|"), end = " ")
-            elif collumns == 2:
-               print("%s !" % ("|"), end = " ")
-            else:
-               print("%s  " % ("|"), end = " ")
-         print("\n-------------------------------------")
+        print("\n-------------------------------------")
+        for row in self.board:
+            for collumns in row:
+                if collumns == 1:
+                    print("%s X" % ("|"), end=" ")
+                elif collumns == 2:
+                    print("%s !" % ("|"), end=" ")
+                else:
+                    print("%s  " % ("|"), end=" ")
+            print("\n-------------------------------------")
 
     def printPlayerMap(self):
         print("\n-------------------------------------")
         for row in self.board:
             for collumns in row:
                 if collumns == 1:
-                    print("%s X" % ("|"), end = " ")
+                    print("%s X" % ("|"), end=" ")
                 elif collumns == 2:
-                    print("%s !" % ("|"), end = " ")
+                    print("%s !" % ("|"), end=" ")
                 elif collumns == 4:
-                    print("%s -" % ("|"), end = " ")
+                    print("%s -" % ("|"), end=" ")
                 else:
-                    print("%s  " % ("|"), end = " ")
+                    print("%s  " % ("|"), end=" ")
             print("\n-------------------------------------")
 
     def hit(self):
-      row = input("Enter Row To Hit: ")
-      collumn = input("Enter Collumn To Hit: ")
-      try:
-         if [row][collumn] == 4:
-            Board[row][collumn] = 2
-         elif [row][collumn] == 0: 
-            Board[row][collumn] = 1
-         else:
-            print("You already attacked this spot")
-            hit() 
+        row = input("Enter Row To Hit: ")
+        collumn = input("Enter Collumn To Hit: ")
+        try:
+            if [row][collumn] == 4:
+                Board[row][collumn] = 2
+            elif [row][collumn] == 0:
+                Board[row][collumn] = 1
+            else:
+                print("You already attacked this spot")
+                hit()
 
-      except Exception:
-         print("Your value entered is out of bounds")
-         hit()
-             
+        except Exception:
+            print("Your value entered is out of bounds")
+            hit()
 
     def createShip(self):
         try:
             for ship in self.playerArsenal:
                 self.printPlayerMap()
                 if ship.isCreated() == False:
-                    ShipRowStart = int(input("Enter Row For Start Posistion For %s: " % (ship.getName())))
-                    ShipCollumnStart = int(input("Enter Collumn For Start Position For %s: " % (ship.getName())))
-                    shipOrientation = input("Enter Orientation for %s (up down left or right): " % (ship.getName()))
+                    ShipRowStart = int(
+                        input("Enter Row For Start Posistion For %s : " % (ship.getName())))
+                    ShipCollumnStart = int(
+                        input("Enter Collumn For Start Position For %s: " % (ship.getName())))
+                    shipOrientation = input(
+                        "Enter Orientation for %s (up down left or right): " % (ship.getName()))
                     if shipOrientation.lower() == "up":
                         ShipRowEnd = ShipRowStart + ship.getSize()
                         ShipCollumnEnd = ShipCollumnStart
@@ -71,18 +74,18 @@ class player:
                             i = i+1
 
                     elif shipOrientation.lower() == "down":
-                        ShipRowEnd = ShipRowStart - ship.getSize()
+                        ShipRowEnd = ShipRowStart + ship.getSize()
                         ShipCollumnEnd = ShipCollumnStart
                         i = ShipRowStart
-                        while i > ShipRowEnd:
+                        while i < ShipRowEnd:
                             self.board[i][ShipCollumnEnd] = 4
-                            i = i-1
+                            i = i+1
 
                     elif shipOrientation.lower() == "left":
                         ShipRowEnd = ShipRowStart
                         ShipCollumnEnd = ShipCollumnStart - ship.getSize()
                         i = ShipCollumnStart
-                        while i > ShipCollumnStart:
+                        while i > ShipCollumnEnd:
                             self.board[ShipRowEnd][i] = 4
                             i = i-1
 
@@ -90,19 +93,23 @@ class player:
                         ShipRowEnd = ShipRowStart
                         ShipCollumnEnd = ShipCollumnStart + ship.getSize()
                         i = ShipCollumnStart
-                        while i < ShipRowEnd:
+                        while i < ShipCollumnEnd:
                             self.board[ShipRowEnd][i] = 4
                             i = i+1
 
                     else:
-                        print("Invalid Ship Location")    
+                        print("Invalid Ship Location")
                         createShip()
 
-                    ship.setShipPos(ShipRowStart, ShipCollumnStart, ShipRowEnd, ShipCollumnEnd)   #set ship location inside of ships.py
-                    ship.printLocation() #print ship location for user to see
+                    # set ship location inside of ships.py
+                    ship.setShipPos(ShipRowStart, ShipCollumnStart,
+                                    ShipRowEnd, ShipCollumnEnd)
+                    ship.printLocation()  # print ship location for user to see
 
                 else:
                     continue
         except Exception:
             print("Invalid Ship Location")
+            self.board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
             self.createShip()
--- ships.py
+++ ships.py
@@ -1,151 +1,156 @@
 class ship:
-   def __init__(self, shipName):
-      self.name = shipName
-   
-   def getName(self):
-      return self.name
+    def __init__(self, shipName):
+        self.name = shipName
+
+    def getName(self):
+        return self.name
+
 
 class carrier(ship):
-   def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip = False):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
-      self.size = 5
-      self.created = creatingShip
-      ship.__init__(self, "Carrier")
-   
+    def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip=False):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
+        self.size = 5
+        self.created = creatingShip
+        ship.__init__(self, "Carrier")
 
-   def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
+    def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
 
+    def printLocation(self):
+        print("  %s is at location (%s,%s) to (%s, %s)." % (
+            self.name, self.ShipCollumnStart, self.ShipRowStart, self.ShipCollumnEnd, self.ShipRowEnd))
 
-   def printLocation(self):
-      print("  %s is at location (%s,%s) to (%s, %s)." % (self.name, self.ShipRowStart, self.ShipCollumnStart, self.ShipRowEnd, self.ShipCollumnEnd))
+    def getSize(self):
+        return self.size
 
-   
-   def getSize(self):
-      return self.size
+    def setSize(self, size):
+        self.size = size
 
-   
-   def setSize(self, size):
-      self.size = size
-   
-   
-   def isCreated(self):
-      return self.created
-
+    def isCreated(self):
+        return self.created
 
 
 class battleShip(ship):
-   def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip = False):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
-      self.size = 4
-      self.created = creatingShip
-      ship.__init__(self, "BattleShip")
+    def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip=False):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
+        self.size = 4
+        self.created = creatingShip
+        ship.__init__(self, "BattleShip")
 
-   def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
+    def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
 
-   def getSize(self):
-      return self.size
+    def printLocation(self):
+        print("  %s is at location (%s,%s) to (%s, %s)." % (
+            self.name, self.ShipCollumnStart, self.ShipRowStart, self.ShipCollumnEnd, self.ShipRowEnd))
 
+    def getSize(self):
+        return self.size
 
-   def setSize(self, size):
-      self.size = size
+    def setSize(self, size):
+        self.size = size
 
-
-   def isCreated(self):
-      return self.created
+    def isCreated(self):
+        return self.created
 
 
 class cruiser(ship):
-   def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip = False):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
-      self.size = 3
-      self.created = creatingShip
-      ship.__init__(self, "Cruiser")
-   
-   def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
+    def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip=False):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
+        self.size = 3
+        self.created = creatingShip
+        ship.__init__(self, "Cruiser")
 
-   def getSize(self):
-      return self.size
+    def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
 
+    def printLocation(self):
+        print("  %s is at location (%s,%s) to (%s, %s)." % (
+            self.name, self.ShipCollumnStart, self.ShipRowStart, self.ShipCollumnEnd, self.ShipRowEnd))
 
-   def setSize(self, size):
-      self.size = size
+    def getSize(self):
+        return self.size
 
-   def isCreated(self):
-      return self.created
+    def setSize(self, size):
+        self.size = size
+
+    def isCreated(self):
+        return self.created
 
 
 class submarine(ship):
-   def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip = False):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
-      self.size = 3
-      self.created = creatingShip
-      ship.__init__(self, "Submarine")
-   
+    def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip=False):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
+        self.size = 3
+        self.created = creatingShip
+        ship.__init__(self, "Submarine")
 
-   def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
+    def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
 
-   def getSize(self):
-      return self.size
+    def printLocation(self):
+        print("  %s is at location (%s,%s) to (%s, %s)." % (
+            self.name, self.ShipCollumnStart, self.ShipRowStart, self.ShipCollumnEnd, self.ShipRowEnd))
 
+    def getSize(self):
+        return self.size
 
-   def setSize(self, size):
-      self.size = size
+    def setSize(self, size):
+        self.size = size
 
-   def isCreated(self):
-      return self.created
+    def isCreated(self):
+        return self.created
 
 
 class destroyer(ship):
-   def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip = False):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
-      self.size = 2
-      self.created = creatingShip
-      ship.__init__(self, "Destroyer")
+    def __init__(self, RowStart, CollumnStart, RowEnd, CollumnEnd, creatingShip=False):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
+        self.size = 2
+        self.created = creatingShip
+        ship.__init__(self, "Destroyer")
 
-   def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
-      self.ShipRowStart = RowStart
-      self.ShipCollumnStart = CollumnStart
-      self.ShipRowEnd = RowEnd
-      self.ShipCollumnEnd = CollumnEnd
+    def setShipPos(self, RowStart, CollumnStart, RowEnd, CollumnEnd):
+        self.ShipRowStart = RowStart
+        self.ShipCollumnStart = CollumnStart
+        self.ShipRowEnd = RowEnd
+        self.ShipCollumnEnd = CollumnEnd
 
+    def printLocation(self):
+        print("  %s is at location (%s,%s) to (%s, %s)." % (
+            self.name, self.ShipCollumnStart, self.ShipRowStart, self.ShipCollumnEnd, self.ShipRowEnd))
 
-   def getSize(self):
-      return self.size
+    def getSize(self):
+        return self.size
 
+    def setSize(self, size):
+        self.size = size
 
-   def setSize(self, size):
-      self.size = size
-
-   def isCreated(self):
-      return self.created
+    def isCreated(self):
+        return self.created
